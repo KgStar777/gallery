749d8fbad3483d532737fba4b7c9d0abfec77e62
KgStar777 <65773394+KgStar777@users.noreply.github.com>
2025-01-16 22:33:35 +0300

    Create

---
+++ backend/config/cors.ts
@@ -0,0 +1,5 @@
+module.exports = {
+  origin: ['http://109.68.214.215:3000', 'http://localhost:3000'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  headers: ['Content-Type', 'Authorization'],
+};
