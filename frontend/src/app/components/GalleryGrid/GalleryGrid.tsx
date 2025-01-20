43bb2436d2874047b3ec035614a6c0dda694e315
Daniil Sychev <sych94@yandex.ru>
2025-01-20 03:06:17 +0300

    grid upd

--- frontend/src/app/components/GalleryGrid/GalleryGrid.tsx
+++ frontend/src/app/components/GalleryGrid/GalleryGrid.tsx
@@ -39,7 +39,7 @@ export function GalleryGrid({
           return (
             <div key={index}>
               {(columnNode as Array<ImageGalleryModel>).map((image, pindex) => {
-                const paint = isMobile ? image.Paint?.formats?.small : image.Paint?.formats?.medium
+                const paint = isMobile ? image.Paint?.formats?.medium : image.Paint?.formats?.large
                 return (
                   // <Link key={pindex} href={image.documentId}>
                   <Link key={pindex} href={`/gallery?id=${image.documentId}`}>
